9fc1446e54fdbef084c7bef930fefd12f1406f1c
corvino79 <corvino_79@hotmail.com>
2016-12-14 00:54:12 -0600

    rri config

--- app/config/index.js
+++ app/config/index.js
@@ -1,5 +1,5 @@
 let config = {
-  API_BASE_URL: process.env.API_BASE_URL || 'https://rri.herokuapp.com'
+  API_BASE_URL: process.env.API_BASE_URL || 'http://rri.herokuapp.com'
 }
 
 export default config;
